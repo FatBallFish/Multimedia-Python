0e48e609ab2bd312b937f779074e643138910a27
FatBallFish <893721708@qq.com>
2019-08-14 22:44:42 +0800

    change again

---
+++ start.bat
@@ -0,0 +1,4 @@
+echo updating backend code
+start git pull origin master
+echo reload backend-server
+start python.exe main.py -c config.ini
--- webhook/start.bat
+++
@@ -1,2 +0,0 @@
-taskkill /F /IM pythonw.exe 
-start pythonw.exe main.py -c config.ini
