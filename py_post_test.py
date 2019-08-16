81f06118deea296c153cfdbfbe541c1ab6f122e3
FatBallFish <893721708@qq.com>
2019-08-16 11:18:44 +0800

    change config.ini

--- py_post_test.py
+++ py_post_test.py
@@ -32,13 +32,13 @@ headers = {'content-type': "application/json"}
 # response = requests.post(url="https://dmt.lcworkroom.cn/api/user/info?token={}".format(token),data=json.dumps(data),headers=headers)
 # response = requests.post(url="http://127.0.0.1:8888/user/info?token={}".format(token),data=json.dumps(data),headers=headers)
 
-# # 增加文章
-# data={"id":0,
-#       "type":"article",
-#       "subtype":"add",
-#       "data":{"title":"测试文章","content":"这是一篇测试文章"}}
-# token = "99c9150238fa21051f558ceccad55b8a"
-# # response = requests.post(url="https://dmt.lcworkroom.cn/api/user/info?token={}".format(token),data=json.dumps(data),headers=headers)
+# 增加文章
+data={"id":0,
+      "type":"article",
+      "subtype":"add",
+      "data":{"title":"测试文章","content":"这是一篇测试文章"}}
+token = "99c9150238fa21051f558ceccad55b8a"
+response = requests.post(url="https://dmt.lcworkroom.cn/api/article?token={}".format(token),data=json.dumps(data),headers=headers)
 # response = requests.post(url="http://127.0.0.1:8765/article?token={}".format(token),data=json.dumps(data),headers=headers)
 
 # # 更新文章
@@ -47,15 +47,15 @@ headers = {'content-type': "application/json"}
 #       "subtype":"update",
 #       "data":{"article_id":1565920055,"content":"这是一篇测试文章,测试一下更新效果"}}
 # token = "99c9150238fa21051f558ceccad55b8a"
-# # response = requests.post(url="https://dmt.lcworkroom.cn/api/user/info?token={}".format(token),data=json.dumps(data),headers=headers)
+# # response = requests.post(url="https://dmt.lcworkroom.cn/api/article?token={}".format(token),data=json.dumps(data),headers=headers)
 # response = requests.post(url="http://127.0.0.1:8765/article?token={}".format(token),data=json.dumps(data),headers=headers)
 
-# 删除文章
-data={"id":0,
-      "type":"article",
-      "subtype":"delete",
-      "data":{"article_id":1565920055}}
-token = "99c9150238fa21051f558ceccad55b8a"
-# response = requests.post(url="https://dmt.lcworkroom.cn/api/user/info?token={}".format(token),data=json.dumps(data),headers=headers)
-response = requests.post(url="http://127.0.0.1:8765/article?token={}".format(token),data=json.dumps(data),headers=headers)
+# # 删除文章
+# data={"id":0,
+#       "type":"article",
+#       "subtype":"delete",
+#       "data":{"article_id":1565920055}}
+# token = "99c9150238fa21051f558ceccad55b8a"
+# # response = requests.post(url="https://dmt.lcworkroom.cn/api/article?token={}".format(token),data=json.dumps(data),headers=headers)
+# response = requests.post(url="http://127.0.0.1:8765/article?token={}".format(token),data=json.dumps(data),headers=headers)
 print(response.text)
